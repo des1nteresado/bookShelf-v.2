405fe44ce96b53d568f9865e7d073b9dc7fd3f08
Daniil Chernyshev <des1nteresado@outlook.com>
2019-04-22 22:02:05 +0300

    Now Index(a method) include partial view

--- FirstApp/Controllers/HomeController.cs
+++ FirstApp/Controllers/HomeController.cs
@@ -22,7 +22,8 @@ namespace FirstApp.Controllers
             // передаем все объекты в динамическое свойство Books в ViewBag
             //ViewBag.Books = books;
             // возвращаем представление
-
+            ViewBag.Message =
+                "Ok. This is message from partial view! But this message create in Index action method! Think about it..";
             //используем строго типизированное представление
             return View(books);
         }
@@ -50,6 +51,12 @@ namespace FirstApp.Controllers
             // сохраняем в бд все изменения
             db.SaveChanges();
             return getToday() + " Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        public ActionResult Partial()
+        {
+            ViewBag.Message = "This is partial view!";
+            return PartialView();
         }
 
         public ActionResult GetFile()
