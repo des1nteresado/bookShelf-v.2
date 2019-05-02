436e56fc1aa181402aae50177b89ad6b446c52f8
Daniil Chernyshev <des1nteresado@outlook.com>
2019-05-02 15:31:48 +0300

    Updated edit-view for book-model

--- FirstApp/Models/Book.cs
+++ FirstApp/Models/Book.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace FirstApp.Models
 {
     public class Book
     {
         // ID книги
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
         // название книги
+        [Display(Name = "Название")]
         public string Name { get; set; }
         // автор книги
+        [Display(Name = "Автор")]
         public string Author { get; set; }
         // цена
+        [Display(Name = "Цена")]
         public int Price { get; set; }
     }
 }
